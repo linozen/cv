47f40c2ab6669667d09fcafbf1708bc8bb108ff7
linozen <linus@sehn.dev>
2024-03-08 19:20:48 +0100

    feat: exchange images

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    ...
+  } @ inputs: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShell.x86_64-linux = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        ({
+          pkgs,
+          config,
+          ...
+        }: {
+          # This is your devenv configuration
+          packages = [pkgs.hello];
+
+          languages.typescript.enable = true;
+
+          enterShell = ''
+            hello
+          '';
+
+          processes.run.exec = "hello";
+        })
+      ];
+    };
+  };
+}
