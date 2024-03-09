9e1fe9f7129d9c3f8d54d28cc5c8c597b627d584
linozen <linus@sehn.dev>
2024-03-09 09:15:12 +0100

    feat: add redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async redirects() {
+        return [
+            {
+                // Redirect all all reauest to /refs/** to Exocortex
+                source: '/refs/:path*',
+                destination: 'https://exocortex.sehn.dev/refs/:path*',
+                permanent: true,
+                // Redirect all all reauest to /zettel/** to Exocortex
+                source: '/zettel/:path*',
+                destination: 'https://exocortex.sehn.dev/concepts/:path*',
+                permanent: true,
+            },
+        ];
+    }
+}
