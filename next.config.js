bc78a50943537e9b3dbe28fd0e7ed17185e42f50
linozen <linus@sehn.dev>
2024-03-09 09:22:03 +0100

    fix: add proper redirects

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    async redirects() {
-        return [
-            {
-                // Redirect all all reauest to /refs/** to Exocortex
-                source: '/refs/:path*',
-                destination: 'https://exocortex.sehn.dev/refs/:path*',
-                permanent: true,
-                // Redirect all all reauest to /zettel/** to Exocortex
-                source: '/zettel/:path*',
-                destination: 'https://exocortex.sehn.dev/concepts/:path*',
-                permanent: true,
-            },
-        ];
-    }
-}
+  async redirects() {
+    return [
+      {
+        // Redirect all all reauest to /refs/** to Exocortex
+        source: "/refs/:path*",
+        destination: "https://exocortex.sehn.dev/refs/:path*",
+        permanent: true,
+      },
+      {
+        // Redirect all all reauest to /zettel/** to Exocortex
+        source: "/zettel/:path*",
+        destination: "https://exocortex.sehn.dev/concepts/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
